2525975a046462817dc5d2f06292da764d4c3150
davidychow87 <davidychow87@gmail.com>
2017-07-28 01:11:57 -0700

    init commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import webpack from 'webpack';
 import { isDebug } from '../config/app';
-import { connect } from './db';
+// import { connect } from './db';
 import initPassport from './init/passport';
 import initExpress from './init/express';
 import initRoutes from './init/routes';
@@ -14,7 +14,7 @@ const app = express();
  * - connect to MongoDB using mongoose
  * - register mongoose Schema
  */
-connect();
+// connect();
 
 /*
  * REMOVE if you do not need passport configuration
